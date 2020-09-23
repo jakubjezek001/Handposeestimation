dc3551c1be7de723c58e5a3956dcc1609000f21e
adahiya <a.dahiya@visium.ch>
2020-09-23 16:07:04 +0200

    baseline model updated with better logging and added metrics

--- src/models/baseline_model.py
+++ src/models/baseline_model.py
@@ -3,6 +3,7 @@ import torch
 import torchvision
 from pytorch_lightning.core.lightning import LightningModule
 from torch.nn import functional as F
+from src.visualization.visualize import plot_truth_vs_prediction
 
 
 class BaselineModel(LightningModule):
@@ -33,7 +34,58 @@ class BaselineModel(LightningModule):
         x, y = batch["image"], batch["joints"]
         prediction = self(x)
         loss = F.mse_loss(prediction, y)
-        return {"loss": loss}
+        train_metrics = self.calculate_metrics(prediction, y, step="train")
+        comet_experiment = self.logger.experiment
+        comet_experiment.log_metrics({**{"loss": loss}, **train_metrics})
+        plot_truth_vs_prediction(
+            prediction[0].detach().numpy(), y[0].detach().numpy(), comet_experiment
+        )
+        return {**{"loss": loss}, **train_metrics}
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=1e-3)
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch["image"], batch["joints"]
+        prediction = self(x)
+        loss = F.mse_loss(prediction, y)
+        val_metrics = self.calculate_metrics(prediction, y, step="val")
+        return {**{"val_loss": loss}, **val_metrics}
+
+    def validation_epoch_end(self, outputs):
+        # called at the end of the validation epoch
+        # outputs is an array with what you returned in validation_step for each batch
+        # outputs = [{'loss': batch_0_loss}, {'loss': batch_1_loss}, ..., {'loss': batch_n_loss}]
+        val_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
+        val_epe_mean = torch.stack([x["EPE_mean_val"] for x in outputs]).mean()
+        val_epe_median = torch.stack([x["EPE_median_val"] for x in outputs]).mean()
+        self.logger.experiment.log_metrics(
+            {
+                "val_loss": val_loss,
+                "val_epe_mean": val_epe_mean,
+                "val_epe_median": val_epe_median,
+            }
+        )
+        return {
+            "val_loss": val_loss,
+            "val_epe_mean": val_epe_mean,
+            "val_epe_median": val_epe_median,
+        }
+
+    def calculate_metrics(self, y_pred, y_true, step: str = "train"):
+        # Mean distance between predicted and true joint.
+        # TODO: Question: Also should these metrics be calculated with prdicted 3D joints or 2.5D predictions?
+        # TODO:  Ask Adrian if it is the mean of the median distance between each hand.
+        # TODO: Problems in unit of these errors as they are scaled versions.
+        # or overall median distance between that batch
+        print(y_pred.shape)
+        distance_joints = (
+            torch.sum(((y_pred - y_true) ** 2), 2) ** 0.5
+        )  # shape: (batch, 21)
+        mean_distance = torch.mean(distance_joints)
+        # mean of the median distances.
+        median_distance = torch.mean(torch.median(distance_joints, 1)[0])
+        return {
+            f"EPE_mean_{step}": mean_distance,
+            f"EPE_median_{step}": median_distance,
+        }
