f7815dfb4f2b45aabc137882fba8eeb9ce14b27f
adahiya <a.dahiya@visium.ch>
2020-09-26 19:07:59 +0200

    constants updated to have data path from environemnt variable

--- src/constants.py
+++ src/constants.py
@@ -2,6 +2,8 @@ import os
 
 
 MASTER_THESIS_DIR = os.environ.get("MASTER_THESIS_PATH")
-FREIHAND_DATA = os.path.join(MASTER_THESIS_DIR, "data", "raw", "FreiHAND_pub_v2")
-PARENT_JOINT = 0
-CHILD_JOINT = 2
+DATA_PATH = os.environ.get("DATA_PATH")
+FREIHAND_DATA = os.path.join(DATA_PATH, "raw", "FreiHAND_pub_v2")
+TRAINING_CONFIG_PATH = os.path.join(
+    MASTER_THESIS_DIR, "src", "experiments", "training_config.json"
+)
