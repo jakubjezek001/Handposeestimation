d2c9955a3182948222c5066ed2fce933ec5c6427
adahiya <a.dahiya@visium.ch>
2020-10-14 20:36:21 +0200

    data reader updated (F_DB" obsolete)

--- src/data_loader/freihand_loader.py
+++ src/data_loader/freihand_loader.py
@@ -1,12 +1,9 @@
 import os
-import random
 from typing import List
 
 import cv2
 import torch
 from src.data_loader.joints import Joints
-from src.data_loader.sample_augmenter import SampleAugmenter
-from src.data_loader.utils import convert_2_5D_to_3D, convert_to_2_5D
 from src.utils import read_json
 from torch.utils.data import Dataset
 
@@ -18,9 +15,7 @@ class F_DB(Dataset):
     Refer to joint_mapping.json in src/data_loader/utils.
     """
 
-    def __init__(
-        self, root_dir: str, labels_path: str, camera_param_path: str, transform, config
-    ):
+    def __init__(self, root_dir: str, labels_path: str, camera_param_path: str, config):
         """Initializes the freihand dataset class, relevant paths and the Joints
         class for remapping of freihand formatted joints to that of AIT.
 
@@ -31,22 +26,12 @@ class F_DB(Dataset):
             transform ([type]): Transforms that needs to be applied to the image.
         """
         self.root_dir = root_dir
-        self.labels = self.get_labels(labels_path)
-        self.camera_param = self.get_camera_param(camera_param_path)
-        self.img_names = self.get_image_names()
-        self.transform = transform
+        self.labels = self.get_labels(labels_path)[:4096]
+        self.camera_param = self.get_camera_param(camera_param_path)[:4096]
+        self.img_names = self.get_image_names()[:4096]
         # To convert from freihand to AIT format.
         self.joints = Joints()
         self.config = config
-        random.seed(self.config.seed)  # To control randomness in rotation
-        self.augment = SampleAugmenter(
-            crop=self.config.crop,
-            resize=self.config.resize,
-            rotate=self.config.rotate,
-            seed=self.config.seed,
-            resize_shape=self.config.resize_shape,
-            crop_margin=self.config.crop_margin,
-        )
 
     def get_image_names(self) -> List[str]:
         """Gets the name of all the files in root_dir.
@@ -84,9 +69,18 @@ class F_DB(Dataset):
     def __len__(self):
         return len(self.img_names)
 
-    def __getitem__(self, idx):
-        # TODO: Write appropriate docstring once the output is finalized.
-        # Remove unnecessary data that is being passed.
+    def __getitem__(self, idx: int) -> dict:
+        """Returns a sample corresponding to the index.
+
+        Args:
+            idx (int): index
+
+        Returns:
+            dict: item with following elements.
+                "image" in opencv bgr format.
+                "K": camera params
+                "joints3D": 3D coordinates of joints in AIT format.
+        """
 
         if torch.is_tensor(idx):
             idx = idx.tolist()
@@ -96,18 +90,6 @@ class F_DB(Dataset):
             torch.tensor(self.labels[idx % 32560]).float()
         )
         camera_param = torch.tensor(self.camera_param[idx % 32560]).float()
-        joints25D, scale = convert_to_2_5D(camera_param, joints3D)
-        # Applying sample related transforms
-        img, joints25D = self.augment.transform_sample(img, joints25D)
-        sample = {
-            "image": img,
-            "joints": joints25D,
-            "scale": scale,
-            "K": camera_param,
-            "joints_3D": joints3D,
-            "joints_3D_recreated": convert_2_5D_to_3D(joints25D, scale, camera_param),
-        }
-        # Applying only image related transform
-        if self.transform:
-            sample["image"] = self.transform(sample["image"])
+
+        sample = {"image": img, "K": camera_param, "joints3D": joints3D}
         return sample
