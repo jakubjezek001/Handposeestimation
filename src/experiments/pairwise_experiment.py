44ab03b2a5cae7465f5e7943a1b706e93b776de4
Aneesh Dahiya <adahiya@cnb-d102-57.inf.ethz.ch>
2020-10-29 18:10:23 +0100

    experiment for the pairwise loss

--- src/experiments/pairwise_experiment.py
+++ src/experiments/pairwise_experiment.py
@@ -1,23 +1,23 @@
 import copy
 import os
-from comet_ml import experiment
+
 from easydict import EasyDict as edict
 from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from pytorch_lightning.loggers import CometLogger
-from src.constants import PAIRWISE_CONFIG, TRAINING_CONFIG_PATH, DATA_PATH
+from src.constants import DATA_PATH, PAIRWISE_CONFIG, TRAINING_CONFIG_PATH
 from src.data_loader.data_set import Data_Set
 from src.data_loader.utils import get_train_val_split
 from src.models.callbacks.upload_comet_logs import UploadCometLogs
 from src.models.pairwise_model import PairwiseModel
-from src.utils import read_json, get_console_logger
+from src.utils import get_console_logger, read_json
 from torchvision import transforms
 
 
 def main():
-    seed_everything()
+
     train_param = edict(read_json(TRAINING_CONFIG_PATH))
-    train_param.epochs = 150
+    train_param.epochs = 100
     train_param.batch_size = 64
     train_param.augmentation_flags = {
         "color_drop": False,
@@ -41,6 +41,7 @@ def main():
         "resize": True,
         "rotate": True,
     }
+    seed_everything(train_param.seed)
     train_data = Data_Set(
         config=train_param,
         transform=transforms.ToTensor(),
@@ -66,6 +67,7 @@ def main():
     # model
 
     model_param = edict(read_json(PAIRWISE_CONFIG))
+    model_param.batch_size = train_param.batch_size
     model_param.num_samples = len(train_data)
     model = PairwiseModel(model_param)
 
@@ -74,11 +76,12 @@ def main():
         "step", get_console_logger("callback"), "pairwise"
     )
     lr_monitor = LearningRateMonitor(logging_interval="step")
-
+    checkpoint_callback = ModelCheckpoint(save_top_k=-1, period=10)
     # trainer
     trainer = Trainer(
         precision=16,
         logger=comet_logger,
+        checkpoint_callback=checkpoint_callback,
         callbacks=[upload_comet_logs, lr_monitor],
         gpus="0",
         max_epochs=train_param.epochs,
