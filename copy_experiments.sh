7ea01186170f93de6d33aa3b12a6987c9a7ab8b3
Aneesh Dahiya <adahiya@cnb-d102-57.inf.ethz.ch>
2020-11-28 17:56:25 +0100

    bug in the script fixed

--- copy_experiments.sh
+++ copy_experiments.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 # VARIABLES
-FOLDER="d2a27fcaa2f6412cba59fc21126ac571"
+FOLDER="d818d4d306954c1dbcfb24cb30f55c8e"
 FROM_CLUSTER=true
 
 # Hidden variables
 
-_help_description="
-Script to transfer the experiment from LabPC to leonhard and vice a versa
+_help_description="Script to transfer the experiment from LabPC to leonhard and vice a versa
+To transfer from  pc to cluster
+bash $0 <experiment_name> --pc 
+To trasnfer from cluster to pc:
+bash $0 <experiment_name> --cluster
 "
 
 
 if [ $# -eq 0 ]; then
     echo "No folder provided!"
+elif [[ $1 == "-h"  ]]; then
+    echo "$_help_description"
+            exit 0
 else
+    FOLDER="$1"
+    shift
     while [ -n "$1" ]; do
         case "$1" in
-        -cluster)
+        --cluster)
             FROM_CLUSTER=true
             shift
             ;;
-        -pc)
+        --pc)
             FROM_CLUSTER=false
             shift
-            ;;
-        -folder)
-            FOLDER="$2"
-            shift
-            ;;
-        -h)
-            echo "$_help_description"
-            exit 0
             ;;
         *)
             echo "Option $1 not recognized"
@@ -42,15 +42,16 @@ else
     done
 fi
 
-# Main process
-if $FROM_CLUSTER eq true; then
-    echo "Copying experiment folder from cluster"
-    scp -r  \
-    "adahiya@login.leonhard.ethz.ch:/cluster/scratch/adahiya/data/models/master-thesis/$FOLDER" \
-    "data/models/master-thesis/$FOLDER"
-else
-    echo "Copying experiment folder from Lab PC"
-    scp -r  \
-    "data/models/master-thesis/$FOLDER" \
-    "adahiya@login.leonhard.ethz.ch:/cluster/scratch/adahiya/data/models/master-thesis/$FOLDER"   
-fi
+echo $FOLDER $FROM_CLUSTER
+# # Main process
+# if $FROM_CLUSTER eq true; then
+#     echo "Copying experiment folder from cluster"
+#     scp -r  \
+#     "adahiya@login.leonhard.ethz.ch:/cluster/scratch/adahiya/data/models/master-thesis/$FOLDER" \
+#     "data/models/master-thesis/$FOLDER"
+# else
+#     echo "Copying experiment folder from Lab PC"
+#     scp -r  \
+#     "data/models/master-thesis/$FOLDER" \
+#     "adahiya@login.leonhard.ethz.ch:/cluster/scratch/adahiya/data/models/master-thesis/$FOLDER"   
+# fi
