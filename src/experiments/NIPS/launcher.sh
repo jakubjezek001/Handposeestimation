ff272c905cb07c6e2522c105f1664684713c3354
aneesh <aneeshdahiyaa482@gamil.com>
2020-12-09 20:45:56 +0100

    launcher and experiment script added

--- src/experiments/NIPS/launcher.sh
+++ src/experiments/NIPS/launcher.sh
@@ -26,7 +26,7 @@ OPTIONS:
 # $2 : cpu cores
 # $3 : augmentation
 # $4 : GPU model
-launch_experimentA1 () {
+launch_experimentA1() {
     # Launching on Cluster.
     echo "Augmentation: $3 "
     # echo " bsub -J A1_$3 -W $1:00 \-o /cluster/scratch//adahiya/nipsa1_$3_logs.out \
@@ -34,10 +34,10 @@ launch_experimentA1 () {
     # -G ls_infk \
     # python src/experiments/NIPS/nips_A1_experiment.py $3"
     bsub -J "A1_$3" -W "$1:00" \-o "/cluster/scratch//adahiya/nipsa1_$3_logs.out" \
-    -n $2 -R "rusage[mem=$MEMORY, ngpus_excl_p=1]" \
-    -R  "select[gpu_model0==$4]" \
-    -G ls_infk \
-    python src/experiments/NIPS/nips_A1_experiment.py $3
+        -n $2 -R "rusage[mem=$MEMORY, ngpus_excl_p=1]" \
+        -R "select[gpu_model0==$4]" \
+        -G ls_infk \
+        python src/experiments/NIPS/nips_A1_experiment.py $3
 
 }
 # To launch experiment 1A on cluster.
@@ -46,18 +46,14 @@ launch_experimentA1 () {
 # $2 : cpu cores
 # $3 : augmentation
 # $4 : GPU model
-launch_experimentA2 () {
+launch_experimentA2() {
     # Launching on Cluster.
     echo "Augmentation: $3 "
-    # echo " bsub -J A1_$3 -W $1:00 \-o /cluster/scratch//adahiya/nipsa1_$3_logs.out \
-    # -n $2 -R 'rusage[mem=7892, ngpus_excl_p=1 gpu_model0==$4]' \
-    # -G ls_infk \
-    # python src/experiments/NIPS/nips_A1_experiment.py $3"
     bsub -J "A2_$3" -W "$1:00" \-o "/cluster/scratch//adahiya/nipsa2_$3_logs.out" \
-    -n $2 -R "rusage[mem=$MEMORY, ngpus_excl_p=1]" \
-    -R  "select[gpu_model0==$4]" \
-    -G ls_infk \
-    python src/experiments/NIPS/nips_A2_experiment.py $3
+        -n $2 -R "rusage[mem=$MEMORY, ngpus_excl_p=1]" \
+        -R "select[gpu_model0==$4]" \
+        -G ls_infk \
+        python src/experiments/NIPS/nips_A2_experiment.py $3
 
 }
 # To launch experiment 1A on cluster.
@@ -68,14 +64,14 @@ launch_experimentA2 () {
 # $4 : experiment_key
 # $5 : experiment_name
 # $6 : experiment_type
-launch_experimentA_downstream () {
+launch_experimentA_downstream() {
     # Launching on Cluster.
     echo "Experiment $5 downstream experiment submitted!"
     bsub -J "A1_$5" -W "$1:00" \-o "/cluster/scratch//adahiya/ssl_$5_logs.out" \
-    -n $2 -R "rusage[mem=$MEMORY, ngpus_excl_p=1]" \
-    -R  "select[gpu_model0==$3]" \
-    -G ls_infk \
-    python src/experiments/NIPS/downstream_experiment.py $4 $5 $6
+        -n $2 -R "rusage[mem=$MEMORY, ngpus_excl_p=1]" \
+        -R "select[gpu_model0==$3]" \
+        -G ls_infk \
+        python src/experiments/NIPS/downstream_experiment.py $4 $5 $6
 
 }
 
@@ -84,22 +80,58 @@ launch_experimentA_downstream () {
 # $1 : time
 # $2 : cpu cores
 # $3 : GPU model
-launch_imagenet_downstream () {
+launch_imagenet_downstream() {
     # Launching on Cluster.
     echo "Experiment Imagenet downstream experiment submitted!"
     bsub -J "imagenet_downstream" -W "$1:00" \-o "/cluster/scratch//adahiya/ssl_imagenet_logs.out" \
-    -n $2 -R "rusage[mem=$MEMORY, ngpus_excl_p=1]" \
-    -R  "select[gpu_model0==$3]" \
-    -G ls_infk \
-    python src/experiments/NIPS/downstream_experiment.py imagenet imagenet IMAGENET
+        -n $2 -R "rusage[mem=$MEMORY, ngpus_excl_p=1]" \
+        -R "select[gpu_model0==$3]" \
+        -G ls_infk \
+        python src/experiments/NIPS/downstream_experiment.py imagenet imagenet IMAGENET
 
 }
+# To launch experiment 1A on cluster.
+# Arguments:
+# $1 : time
+# $2 : cpu cores
+# $3 : GPU model
+# $4 : epochs
+# $5 : accumulate_grad_batches
+launch_experimentB() {
+    # Launching on Cluster.
+    command=" -n $2 -R 'rusage[mem=$MEMORY, ngpus_excl_p=1]' \
+            -R  'select[gpu_model0==$3]' -G ls_infk \
+            python src/experiments/hybrid1_experiment.py -batch_size 512 \
+            -accumulate_grad_batches $5 -epochs $4 "
+    declare -a contrastive_augment=("-contrastive color_jitter"
+        "-contrastive rotate"
+        "-contrastive crop"
+        "-contrastive crop -contrastive color_jitter"
+        "-contrastive crop -contrastive rotate"
+        "-contrastive color_jitter -contrastive rotate"
+        "-contrastive crop -contrastive rotate -contrastive color_jitter")
+    declare -a pairwise_augment=("-pairwise color_jitter"
+        "-pairwise rotate"
+        "-pairwise crop"
+        "-pairwise crop -pairwise color_jitter"
+        "-pairwise crop -pairwise rotate"
+        "-pairwise color_jitter -pairwise rotate"
+        "-pairwise crop -pairwise rotate -pairwise color_jitter")
+    K=1
+    for i in "${contrastive_augment[@]}"; do
+        for j in "${pairwise_augment[@]}"; do
+            echo "bsub -J 'HY1_$K' -W '$1:00' -o '/cluster/scratch//adahiya/nipsB_$K_logs.out' \
+                    $command $j $i"
+            K=$(($K + 1))
+        done
+    done
 
+}
 if [ $# -eq 0 ]; then
     echo "No Experiment selected!"
-elif [[ $1 == "-h"  ]]; then
+elif [[ $1 == "-h" ]]; then
     echo "$_help_description"
-            exit 0
+    exit 0
 else
     EXPERIMENT="$1"
     echo $EXPERIMENT
@@ -132,72 +164,90 @@ else
     done
 fi
 
-
 # Main process
 case $EXPERIMENT in
-    A1)
-        # moving the reference to old experiments in bkp file to make space for new series. 
-        mv "$DATA_PATH/models/nips_A1_experiment" "$DATA_PATH/models/nips_A1_experiment.bkp.$DATE"
-        echo "Launching NIPS experiment A1 . Ablative studies for SIMCLR."
-        launch_experimentA1 $TIME $CORES "color_drop" $GPU_MODEL
-        launch_experimentA1 $TIME $CORES "color_jitter" $GPU_MODEL
-        launch_experimentA1 $TIME $CORES "crop" $GPU_MODEL # Translation jitter.
-        launch_experimentA1 $TIME $CORES "cut_out" $GPU_MODEL
-        launch_experimentA1 $TIME $CORES "gaussian_blur" $GPU_MODEL
-        launch_experimentA1 $TIME $CORES "random_crop" $GPU_MODEL
-        launch_experimentA1 $TIME $CORES "rotate" $GPU_MODEL
-        launch_experimentA1 $TIME $CORES "gaussian_noise" $GPU_MODEL
-        launch_experimentA1 $TIME $CORES "sobel_filter" $GPU_MODEL
-         # sanity check no augmentation
-        launch_experimentA1 $TIME $CORES "resize" $GPU_MODEL
-        ;;
-    A1_DOWN)
-        mv "$DATA_PATH/models/nips_A_downstream" "$DATA_PATH/models/nips_A1_downstream.bkp.$DATE"
-        echo "Launching downstream experiments for SIMCLR ablative studies."
-        while IFS=',' read -r experiment_name experiment_key
-            do
-            launch_experimentA_downstream $TIME $CORES $GPU_MODEL $experiment_key $experiment_name "NIPS_A1"
-            done < $DATA_PATH/models/nips_A1_experiment
-        ;;
-    A2)
-        # moving the reference to old experiments in bkp file to make space for new series. 
-        mv "$DATA_PATH/models/nips_A2_experiment" "$DATA_PATH/models/nips_A2_experiment.bkp.$DATE"
-        echo "Launching NIPS experiment A2 . Ablative studies for Pairwise."
-        launch_experimentA2 $TIME $CORES "color_jitter" $GPU_MODEL
-        launch_experimentA2 $TIME $CORES "rotate" $GPU_MODEL
-        launch_experimentA2 $TIME $CORES "crop" $GPU_MODEL 
-        ;;
-        
-    A2_DOWN)
-        mv "$DATA_PATH/models/nips_A2_downstream" "$DATA_PATH/models/nips_A2_downstream.bkp.$DATE"
-        echo "Launching downstream experiments for Pairwise ablative studies."
-        while IFS=',' read -r experiment_name experiment_key
-            do
-            launch_experimentA_downstream $TIME $CORES $GPU_MODEL $experiment_key $experiment_name "NIPS_A2"
-            done < $DATA_PATH/models/nips_A2_experiment
-        ;;
+A1)
+    # moving the reference to old experiments in bkp file to make space for new series.
+    mv "$DATA_PATH/models/nips_A1_experiment" "$DATA_PATH/models/nips_A1_experiment.bkp.$DATE"
+    echo "Launching NIPS experiment A1 . Ablative studies for SIMCLR."
+    launch_experimentA1 $TIME $CORES "color_drop" $GPU_MODEL
+    launch_experimentA1 $TIME $CORES "color_jitter" $GPU_MODEL
+    launch_experimentA1 $TIME $CORES "crop" $GPU_MODEL # Translation jitter.
+    launch_experimentA1 $TIME $CORES "cut_out" $GPU_MODEL
+    launch_experimentA1 $TIME $CORES "gaussian_blur" $GPU_MODEL
+    launch_experimentA1 $TIME $CORES "random_crop" $GPU_MODEL
+    launch_experimentA1 $TIME $CORES "rotate" $GPU_MODEL
+    launch_experimentA1 $TIME $CORES "gaussian_noise" $GPU_MODEL
+    launch_experimentA1 $TIME $CORES "sobel_filter" $GPU_MODEL
+    # sanity check no augmentation
+    launch_experimentA1 $TIME $CORES "resize" $GPU_MODEL
+    ;;
+A1_DOWN)
+    mv "$DATA_PATH/models/nips_A_downstream" "$DATA_PATH/models/nips_A1_downstream.bkp.$DATE"
+    echo "Launching downstream experiments for SIMCLR ablative studies."
+    while IFS=',' read -r experiment_name experiment_key; do
+        launch_experimentA_downstream $TIME $CORES $GPU_MODEL $experiment_key $experiment_name "NIPS_A1"
+    done <$DATA_PATH/models/nips_A1_experiment
+    ;;
+A2)
+    # moving the reference to old experiments in bkp file to make space for new series.
+    mv "$DATA_PATH/models/nips_A2_experiment" "$DATA_PATH/models/nips_A2_experiment.bkp.$DATE"
+    echo "Launching NIPS experiment A2 . Ablative studies for Pairwise."
+    launch_experimentA2 $TIME $CORES "color_jitter" $GPU_MODEL
+    launch_experimentA2 $TIME $CORES "rotate" $GPU_MODEL
+    launch_experimentA2 $TIME $CORES "crop" $GPU_MODEL
+    ;;
 
-    IMAGENET_DOWN)
-        echo "Launching downstream experiments for trained Imagenet."
-        launch_imagenet_downstream $TIME $CORES $GPU_MODEL
-        ;;
-    
-    SUPERVISED)
-        epochs="50"
-        num_workers="12"
-        batch_size="128"
-        echo "Launching baseline experiment forn $epochs epochs "
+A2_DOWN)
+    mv "$DATA_PATH/models/nips_A2_downstream" "$DATA_PATH/models/nips_A2_downstream.bkp.$DATE"
+    echo "Launching downstream experiments for Pairwise ablative studies."
+    while IFS=',' read -r experiment_name experiment_key; do
+        launch_experimentA_downstream $TIME $CORES $GPU_MODEL $experiment_key $experiment_name "NIPS_A2"
+    done <$DATA_PATH/models/nips_A2_experiment
+    ;;
+
+IMAGENET_DOWN)
+    echo "Launching downstream experiments for trained Imagenet."
+    launch_imagenet_downstream $TIME $CORES $GPU_MODEL
+    ;;
+
+SUPERVISED)
+    epochs="50"
+    num_workers="12"
+    batch_size="128"
+    echo "Launching baseline experiment for $epochs epochs "
+    bsub -J "imagenet_downstream" -W "$TIME:00" \-o "/cluster/scratch//adahiya/supervised_logs.out" \
+        -n $CORES -R "rusage[mem=$MEMORY, ngpus_excl_p=1]" \
+        -R "select[gpu_model0==$GPU_MODEL]" \
+        -G ls_infk \
         python src/experiments/baseline_experiment.py --rotate --crop --resize \
-            -epochs $epochs -batch_size $batch_size -num_workers $num_workers
-        ;;
-        
-    *)
-        echo "Experiment not recognized!"
-        echo "(Run $0 -h for help)"
-        exit -1
-        ;;
-
-
+        -epochs $epochs -batch_size $batch_size -num_workers $num_workers
+    ;;
+HYBRID1)
+    epoch="300"
+    num_workers="12"
+    batch_size="512"
+    accumulate_grad_batches="4"
+    echo "Launching Hybrid 1 experiment for $epochs epochs"
+    bsub -J "Hybrid_00" -W "$TIME:00" \-o "/cluster/scratch//adahiya/hybrid00_logs.out" \
+        -n $CORES -R "rusage[mem=$MEMORY, ngpus_excl_p=1]" \
+        -R "select[gpu_model0==$GPU_MODEL]" \
+        -G ls_infk \
+        python src/experiments/hybrid1_experiment.py -contrastive "color_jitter" \
+        -pairwise "rotate" -pairwise "crop" \
+        -epochs $epochs -batch_size $batch_size -num_workers $num_workers \
+        -accumulate_grad_batches $accumulate_grad_batches
+    ;;
+NIPS_B)
+    epochs="100"
+    accumulate_grad_batches="4"
+    launch_experimentB $TIME $CORES $GPU_MODEL $epochs $accumulate_grad_batches
+    ;;
+*)
+    echo "Experiment not recognized!"
+    echo "(Run $0 -h for help)"
+    exit -1
+    ;;
 esac
 
 echo "All experiment successfully launched!"
---
+++ src/experiments/hybrid1_experiment.py
@@ -0,0 +1,108 @@
+import os
+from pprint import pformat
+
+from easydict import EasyDict as edict
+from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from pytorch_lightning.loggers import CometLogger
+from src.constants import (
+    DATA_PATH,
+    HYBRID1_CONFIG,
+    HYBRID1_AUGMENTATION_CONFIG,
+    MASTER_THESIS_DIR,
+    TRAINING_CONFIG_PATH,
+)
+from src.data_loader.data_set import Data_Set
+from src.data_loader.utils import get_train_val_split
+from src.experiments.utils import (
+    get_hybrid1_args,
+    prepare_name,
+    update_hybrid1_train_args,
+)
+from src.models.callbacks.upload_comet_logs import UploadCometLogs
+from src.models.hybrid1_model import Hybrid1Model
+from src.utils import get_console_logger, read_json
+from torchvision import transforms
+
+
+def main():
+    # get configs
+    console_logger = get_console_logger(__name__)
+    args = get_hybrid1_args("Hybrid model 1 training script.")
+
+    train_param = edict(read_json(TRAINING_CONFIG_PATH))
+    train_param.update(edict(read_json(HYBRID1_AUGMENTATION_CONFIG)))
+    train_param.pairwise.augmentation_flags.resize = True
+    train_param.contrastive.augmentation_flags.resize = True
+    train_param = update_hybrid1_train_args(args, train_param)
+    model_param = edict(read_json(HYBRID1_CONFIG))
+    console_logger.info(f"Train parameters {pformat(train_param)}")
+    seed_everything(train_param.seed)
+
+    data = Data_Set(
+        config=train_param,
+        transform=transforms.ToTensor(),
+        train_set=True,
+        experiment_type="hybrid1",
+    )
+    train_data_loader, val_data_loader = get_train_val_split(
+        data,
+        batch_size=train_param.batch_size,
+        num_workers=train_param.num_workers,
+        shuffle=True,
+    )
+
+    # logger
+    comet_logger = CometLogger(
+        api_key=os.environ.get("COMET_API_KEY"),
+        project_name="master-thesis",
+        workspace="dahiyaaneesh",
+        save_dir=os.path.join(DATA_PATH, "models"),
+        experiment_name=prepare_name("hybrid1_", train_param, hybrid_naming=True),
+    )
+    # model
+    model_param.num_samples = len(data)
+    model_param.batch_size = train_param.batch_size
+    model_param.num_of_mini_batch = train_param.accumulate_grad_batches
+    model_param.contrastive.augmentation = args.contrastive
+    model_param.pairwise.augmentation = args.pairwise
+    console_logger.info(f"Model parameters {pformat(model_param)}")
+    model = Hybrid1Model(model_param)
+
+    # callbacks
+    logging_interval = "epoch"
+    upload_comet_logs = UploadCometLogs(
+        logging_interval, get_console_logger("callback"), "hybrid1"
+    )
+    lr_monitor = LearningRateMonitor(logging_interval=logging_interval)
+    # saving the best model as per the validation loss.
+    checkpoint_callback = ModelCheckpoint(
+        save_top_k=1, period=1, monitor="checkpoint_saving_loss"
+    )
+
+    # trainer
+    trainer = Trainer(
+        accumulate_grad_batches=train_param.accumulate_grad_batches,
+        gpus="0",
+        logger=comet_logger,
+        max_epochs=train_param.epochs,
+        precision=train_param.precision,
+        amp_backend="native",
+        callbacks=[lr_monitor, upload_comet_logs],
+        checkpoint_callback=checkpoint_callback,
+    )
+    trainer.logger.experiment.set_code(
+        overwrite=True,
+        filename=os.path.join(
+            MASTER_THESIS_DIR, "src", "experiments", "hybrid1_experiment.py"
+        ),
+    )
+    trainer.logger.experiment.log_parameters(train_param)
+    trainer.logger.experiment.log_parameters(model_param)
+    trainer.logger.experiment.add_tags(["pretraining", "HYBRID1"] + args.tag)
+    # training
+    trainer.fit(model, train_data_loader, val_data_loader)
+
+
+if __name__ == "__main__":
+    main()
