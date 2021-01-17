705e1e666a0da180e2ff937f5a674958bf10144c
aneesh <aneeshdahiyaa482@gamil.com>
2021-01-17 15:32:15 +0100

    mpii dataset class added

--- src/constants.py
+++ src/constants.py
@@ -5,6 +5,7 @@ DATA_PATH = os.environ.get("DATA_PATH")
 FREIHAND_DATA = os.path.join(DATA_PATH, "freihand_dataset")
 INTERHAND_DATA = os.path.join(DATA_PATH, "InterHand")
 YOUTUBE_DATA = os.path.join(DATA_PATH, "youtube_3d_hands", "data")
+MPII_DATA = os.path.join(DATA_PATH, "mpii_dataset")
 TRAINING_CONFIG_PATH = os.path.join(
     MASTER_THESIS_DIR, "src", "experiments", "config", "training_config.json"
 )
@@ -41,6 +42,13 @@ HYBRID1_AUGMENTATION_CONFIG = os.path.join(
     "experiments",
     "config",
     "hybrid1_augmentation_config.json",
+)
+HRNET_CONFIG = os.path.join(
+    MASTER_THESIS_DIR, "src", "models", "HRnet", "hrnet_config.yaml"
+)
+# TODO: Add appropriate path
+HEATMAP_CONFIG_PATH = os.path.join(
+    MASTER_THESIS_DIR, "src", "experiments", "config", "heatmap_config.json"
 )
 
 ANGLES = [i for i in range(10, 360, 10)]
--- src/data_loader/data_set.py
+++ src/data_loader/data_set.py
@@ -1,5 +1,4 @@
 import os
-import random
 from typing import Tuple
 
 import numpy as np
@@ -7,14 +6,14 @@ import torch
 import torchvision
 from easydict import EasyDict as edict
 from sklearn.model_selection import train_test_split
-from src.constants import FREIHAND_DATA, INTERHAND_DATA, YOUTUBE_DATA
+from src.constants import FREIHAND_DATA, INTERHAND_DATA, YOUTUBE_DATA, MPII_DATA
 from src.data_loader.freihand_loader import F_DB
 from src.data_loader.interhand_loader import IH_DB
 from src.data_loader.youtube_loader import YTB_DB
+from src.data_loader.mpii_loader import MPII_DB
 from src.data_loader.sample_augmenter import SampleAugmenter
 from src.data_loader.utils import convert_2_5D_to_3D, convert_to_2_5D
 from torch.utils.data import Dataset
-from torchvision import transforms
 
 
 class Data_Set(Dataset):
@@ -86,6 +85,13 @@ class Data_Set(Dataset):
             )
         elif self.source == "youtube":
             self.db = YTB_DB(root_dir=YOUTUBE_DATA, split="train")
+            self.train_indices, self.val_indices = train_test_split(
+                np.arange(0, len(self.db)),
+                train_size=self.config.train_ratio,
+                random_state=self.config.seed,
+            )
+        elif self.source == "mpii":
+            self.db = MPII_DB(root_dir=MPII_DATA)
             self.train_indices, self.val_indices = train_test_split(
                 np.arange(0, len(self.db)),
                 train_size=self.config.train_ratio,
