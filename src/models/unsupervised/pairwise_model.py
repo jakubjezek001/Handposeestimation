92c66baf5c96e680985fda510432579beb517e08
aneesh <aneeshdahiyaa482@gamil.com>
2021-01-18 12:08:59 +0100

    pairwise model refactored

--- src/models/unsupervised/pairwise_model.py
+++ src/models/unsupervised/pairwise_model.py
@@ -1,24 +1,20 @@
-import math
+from typing import Dict, Tuple
 
 import torch
-import torchvision
-from pl_bolts.optimizers.lars_scheduling import LARSWrapper
-from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
-from pytorch_lightning.core.lightning import LightningModule
+from src.models.base_model import BaseModel
 from torch import nn
 from torch.nn import L1Loss
+from torch.tensor import Tensor
 
 
-class PairwiseModel(LightningModule):
+class PairwiseModel(BaseModel):
     """
     Pairwise self-supervised model. The transformation parameters are regressed in this
         model.
     """
 
     def __init__(self, config):
-        super().__init__()
-        self.config = config
-        self.encoder = self.get_encoder()
+        super().__init__(config)
         self.regress_rotate = False
         self.regress_jitter = False
         self.regress_color_jitter = False
@@ -32,19 +28,7 @@ class PairwiseModel(LightningModule):
         if "color_jitter" in self.config.augmentation:
             self.color_jitter_head = self.get_color_jitter_head()
 
-        # Variables used by callbacks
-        self.train_metrics_epoch = None
-        self.train_metrics = None
-        self.validation_metrics_epoch = None
-        self.plot_params = None
-
-    def get_encoder(self):
-        encoder = torchvision.models.resnet18(pretrained=True)
-        # Removing the last classification layer.
-        encoder.fc = nn.Sequential()
-        return encoder
-
-    def get_base_transformation_head(self, output_dim):
+    def get_base_transformation_head(self, output_dim: int) -> nn.Sequential:
         return nn.Sequential(
             nn.Linear(
                 self.config.transformation_head_input_dim * 2,
@@ -58,26 +42,33 @@ class PairwiseModel(LightningModule):
             ),
         )
 
-    def get_rotation_head(self):
+    def get_rotation_head(self) -> nn.Sequential:
         self.regress_rotate = True
         self.log_keys += ["loss_rotation", "sigma_rotation"]
         self.log_sigma_rotate = nn.Parameter(torch.zeros(1, 1))
         rotation_head = self.get_base_transformation_head(output_dim=1)
         return rotation_head
 
-    def get_jitter_head(self):
+    def get_jitter_head(self) -> nn.Sequential:
         self.regress_jitter = True
         self.log_keys += ["loss_jitter", "sigma_jitter"]
         self.log_sigma_jitter = nn.Parameter(torch.zeros(1, 1))
         return self.get_base_transformation_head(output_dim=2)
 
-    def get_color_jitter_head(self):
+    def get_color_jitter_head(self) -> nn.Sequential:
         self.regress_color_jitter = True
         self.log_keys += ["loss_color_jitter", "sigma_color_jitter"]
         self.log_sigma_color_jitter = nn.Parameter(torch.zeros(1, 1))
         return self.get_base_transformation_head(output_dim=4)
 
-    def regress_rotation(self, rotation_gt, encoding, loss, log: dict, pred_gt):
+    def regress_rotation(
+        self,
+        rotation_gt: Tensor,
+        encoding: Tensor,
+        loss: Tensor,
+        log: dict,
+        pred_gt: Tensor,
+    ) -> Tensor:
         rotation_pred = self.rotation_head(encoding)
         loss_rotation = L1Loss()(rotation_gt, rotation_pred)
         loss += loss_rotation / torch.exp(self.log_sigma_rotate) + self.log_sigma_rotate
@@ -90,7 +81,14 @@ class PairwiseModel(LightningModule):
         pred_gt.update({"rotation": [rotation_gt, rotation_pred]})
         return loss
 
-    def regress_jittering(self, jitter_gt, encoding, loss, log, pred_gt):
+    def regress_jittering(
+        self,
+        jitter_gt: Tensor,
+        encoding: Tensor,
+        loss: Tensor,
+        log: Dict[str, Tensor],
+        pred_gt: Tensor,
+    ) -> Tensor:
         jitter_pred = self.jitter_head(encoding)
         loss_jitter = L1Loss()(jitter_gt, jitter_pred)
         loss += loss_jitter / torch.exp(self.log_sigma_jitter) + self.log_sigma_jitter
@@ -103,7 +101,14 @@ class PairwiseModel(LightningModule):
         pred_gt.update({"jitter": [jitter_gt, jitter_pred]})
         return loss
 
-    def regress_color_jittering(self, color_jitter_gt, encoding, loss, log, pred_gt):
+    def regress_color_jittering(
+        self,
+        color_jitter_gt: Tensor,
+        encoding: Tensor,
+        loss: Tensor,
+        log: Dict[str, Tensor],
+        pred_gt: Tensor,
+    ) -> Tensor:
         color_jitter_pred = self.color_jitter_head(encoding)
         loss_color_jitter = L1Loss()(color_jitter_gt, color_jitter_pred)
         loss += (
@@ -119,14 +124,16 @@ class PairwiseModel(LightningModule):
         pred_gt.update({"color_jitter": [color_jitter_gt, color_jitter_pred]})
         return loss
 
-    def transformation_regression_step(self, batch):
-        batch_transform1 = batch["transformed_image1"]
-        batch_transform2 = batch["transformed_image2"]
-
-        encoding = torch.cat(
-            (self.encoder(batch_transform1), self.encoder(batch_transform2)), 1
+    def transformation_regression_step(
+        self, batch: Tensor
+    ) -> Tuple[Tensor, Dict[str, Tensor], Dict[str, Tensor]]:
+        batch_images = torch.cat(
+            (batch["transformed_image1"], batch["transformed_image2"]), dim=0
         )
-
+        batch_size = len(batch_images) // 2
+        encoding = self.get_encodings(batch_images)
+        # concatentating wmbeddings of augmented sample pairs
+        encoding = torch.cat((encoding[:batch_size], encoding[batch_size:]), 1)
         loss = 0
         log = {}
         pred_gt = {}
@@ -146,11 +153,14 @@ class PairwiseModel(LightningModule):
             )
         return (loss, log, pred_gt)
 
-    def forward(self, x):
-        embedding = self.encoder(x)
+    def get_encodings(self, batch_images: Tensor) -> Tensor:
+        return self.encoder(batch_images)
+
+    def forward(self, x: Tensor) -> Tensor:
+        embedding = self.get_encodings(x)
         return embedding
 
-    def training_step(self, batch, batch_idx):
+    def training_step(self, batch: Tensor, batch_idx: int) -> Dict[str, Tensor]:
         loss, losses, gt_pred = self.transformation_regression_step(batch)
         self.train_metrics = {**{"loss": loss.detach()}, **losses}
         self.plot_params = {
@@ -162,12 +172,7 @@ class PairwiseModel(LightningModule):
         }
         return {**{"loss": loss}, **losses}
 
-    def training_epoch_end(self, outputs):
-        self.train_metrics_epoch = {
-            k: torch.stack([x[k] for x in outputs]).mean() for k in self.log_keys
-        }
-
-    def validation_step(self, batch, batch_idx):
+    def validation_step(self, batch: Tensor, batch_idx: int) -> Dict[str, Tensor]:
         loss, losses, gt_pred = self.transformation_regression_step(batch)
         self.plot_params = {
             **{
@@ -177,72 +182,3 @@ class PairwiseModel(LightningModule):
             **{"gt_pred": gt_pred},
         }
         return {**{"loss": loss}, **losses}
-
-    def validation_epoch_end(self, outputs):
-
-        self.validation_metrics_epoch = {
-            k: torch.stack([x[k] for x in outputs]).mean()
-            for k in self.log_keys
-            if "loss" in k
-        }
-        self.log("checkpoint_saving_loss", self.validation_metrics_epoch["loss"])
-
-    def exclude_from_wt_decay(
-        self, named_params, weight_decay, skip_list=["bias", "bn"]
-    ):
-        params = []
-        excluded_params = []
-
-        for name, param in named_params:
-            if not param.requires_grad:
-                continue
-            elif any(layer_name in name for layer_name in skip_list):
-                excluded_params.append(param)
-            else:
-                params.append(param)
-
-        return [
-            {"params": params, "weight_decay": weight_decay},
-            {"params": excluded_params, "weight_decay": 0.0},
-        ]
-
-    def setup(self, stage):
-        global_batch_size = self.trainer.world_size * self.config.batch_size
-        self.train_iters_per_epoch = self.config.num_samples // global_batch_size
-
-    def configure_optimizers(self):
-        parameters = self.exclude_from_wt_decay(
-            self.named_parameters(), weight_decay=self.config.opt_weight_decay
-        )
-        optimizer = LARSWrapper(
-            torch.optim.Adam(
-                parameters,
-                lr=self.config.lr
-                * math.sqrt(self.config.batch_size * self.config.num_of_mini_batch),
-            )
-        )
-        warmup_epochs = (
-            self.config.warmup_epochs
-            * self.train_iters_per_epoch
-            // self.config.num_of_mini_batch
-        )
-        max_epochs = (
-            self.trainer.max_epochs
-            * self.train_iters_per_epoch
-            // self.config.num_of_mini_batch
-        )
-
-        linear_warmup_cosine_decay = LinearWarmupCosineAnnealingLR(
-            optimizer,
-            warmup_epochs=warmup_epochs,
-            max_epochs=max_epochs,
-            warmup_start_lr=0,
-            eta_min=0,
-        )
-
-        scheduler = {
-            "scheduler": linear_warmup_cosine_decay,
-            "interval": "step",
-            "frequency": 1,
-        }
-        return [optimizer], [scheduler]
