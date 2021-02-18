041ed735edad21a7859279a06034626e81b972b4
aneesh <aneeshdahiyaa482@gamil.com>
2021-02-18 16:40:58 +0100

    heatmap structure utilized in unsupervised models.

--- src/models/unsupervised/hybrid2_heatmap_model.py
+++ src/models/unsupervised/hybrid2_heatmap_model.py
@@ -1,6 +1,7 @@
-from typing import Dict
+from typing import Dict, Tuple
 
 from easydict import EasyDict as edict
+import torch
 from src.models.unsupervised.hybrid2_model import Hybrid2Model
 from src.models.unsupervised.simclr_heatmap_model import SimCLRHeatmap
 from torch import Tensor
@@ -19,3 +20,16 @@ class Hybrid2HeatmapModel(SimCLRHeatmap, Hybrid2Model):
 
     def contrastive_step(self, batch: Dict[str, Tensor]) -> Tensor:
         return Hybrid2Model.contrastive_step(self, batch)
+
+    def get_transformed_projections(
+        self, batch: Dict[str, Tensor]
+    ) -> Tuple[Tensor, Tensor]:
+        if self.config.preserve_heatmap:
+            return self.get_transformed_heatmaps(batch)
+        else:
+            return Hybrid2Model.get_transformed_projections(batch)
+
+    def get_transformed_heatmaps(
+        self, batch: Dict[str, Tensor]
+    ) -> Tuple[Tensor, Tensor]:
+        raise NotImplementedError
