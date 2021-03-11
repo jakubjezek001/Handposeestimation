b86f43cfaad4c116d12fffc7b8334c241930a513
aneesh <aneeshdahiyaa482@gamil.com>
2021-03-11 18:40:01 +0100

    evaluation pipeline updated

--- src/evaluation/freihand_evaluation.py
+++ src/evaluation/freihand_evaluation.py
@@ -68,20 +68,20 @@ def main():
         train_param.augmentation_flags, train_param.augmentation_params
     )
     # Normalization for BGR mode.
-    transform = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Normalize(
-                (0.485, 0.456, 0.406)[::-1], (0.229, 0.224, 0.225)[::-1]
-            ),
-        ]
-    )
     # transform = transforms.Compose(
     #     [
     #         transforms.ToTensor(),
-    #         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    #         transforms.Normalize(
+    #             (0.485, 0.456, 0.406)[::-1], (0.229, 0.224, 0.225)[::-1]
+    #         ),
     #     ]
     # )
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ]
+    )
 
     data = F_DB(FREIHAND_DATA, split=args.split)
     xyz_pred = []
